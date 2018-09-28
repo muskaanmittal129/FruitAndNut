6097929bdedba86f96b6f82d5b8a4d1f60fc4da2
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2018-09-28 13:20:47 +0530

    Update admin.py

--- FruitAndNut/core/admin.py
+++ FruitAndNut/core/admin.py
@@ -1,12 +1,51 @@
 from django.contrib import admin
 from .models import RecentEvent, FooterAbout, FooterContact, FooterRelatedLinks, LandingPortion
 
+class FooterAboutAdmin(admin.ModelAdmin):
+    search_fields = ['text_data']
+    list_display = ('text_data',)
 
-admin.site.register(RecentEvent)
-admin.site.register(FooterRelatedLinks)
-admin.site.register(FooterContact)
-admin.site.register(FooterAbout)
-admin.site.register(LandingPortion)
+    class Meta:
+        model = FooterAbout
+
+
+class FooterContactAdmin(admin.ModelAdmin):
+    search_fields = ['text_data']
+    list_display = ('text_data',)
+
+    class Meta:
+        model = FooterContact
+
+
+class FooterLinkAdmin(admin.ModelAdmin):
+    search_fields = ['link_name', 'link']
+    list_display = ('link_name', 'link')
+
+    class Meta:
+        model = FooterRelatedLinks
+
+
+class LandingAdmin(admin.ModelAdmin):
+    search_fields = ['slider', 'active']
+    list_display = ('slider', 'active')
+
+    class Meta:
+        model = LandingPortion
+
+
+class RecentEventAdmin(admin.ModelAdmin):
+    search_fields = ['caption', 'date_of_event', 'active']
+    list_display = ('caption', 'date_of_event', 'active')
+
+    class Meta:
+        model = RecentEvent
+
+
+admin.site.register(RecentEvent, RecentEventAdmin)
+admin.site.register(FooterRelatedLinks, FooterLinkAdmin)
+admin.site.register(FooterContact, FooterContactAdmin)
+admin.site.register(FooterAbout, FooterContactAdmin)
+admin.site.register(LandingPortion, LandingAdmin)
 
 # admin.site.register()
 
--- FruitAndNut/core/models.py
+++ FruitAndNut/core/models.py
@@ -11,21 +11,50 @@ class RecentEvent(models.Model):
     active = models.BooleanField(default=True)
     caption = models.CharField(max_length=200, blank=True)
 
+    class Meta:
+        verbose_name = "Recent Event"
+        verbose_name_plural = "Recent Events"
+
 
 class FooterRelatedLinks(models.Model):
     link_name = models.CharField(max_length=30)
     link = models.URLField()
 
+    class Meta:
+        verbose_name = "Footer Related Link"
+        verbose_name_plural = "Footer Related Links"
+
+
+    def __str__(self):
+        return self.link_name
+
 
 class FooterAbout(models.Model):
     text_data = models.TextField(blank=True)
 
+    class Meta:
+        verbose_name = "Footer About"
+        verbose_name_plural = "Footer About"
+
+    def __str__(self):
+        return self.text_data
+
 
 class FooterContact(models.Model):
     text_data = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Footer Contact"
+        verbose_name_plural = "Footer Contact"
+
+    def __str__(self):
+        return self.text_data
 
 
 class LandingPortion(models.Model):
     slider = models.ImageField(upload_to='images/landing_slider/', unique=True)
     active = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name = "Landing Portion"
+        verbose_name_plural = "Landing Portion"
