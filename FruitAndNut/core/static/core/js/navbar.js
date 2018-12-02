1aa324dea74c14356ace2d4f85cc9628aed68ae9
prabhanshuchauhan <prabhanshu.chauhan98@gmail.com>
2018-12-02 16:35:28 +0530

    navbar design improved, submenus added

--- FruitAndNut/core/static/core/js/navbar.js
+++ FruitAndNut/core/static/core/js/navbar.js
@@ -1,12 +1,12 @@
-var navbar = document.getElementById("navbarContent");
-var links = navbar.getElementsByClassName("nav-item");
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// var navbar = document.getElementById("navbarContent");
+// var links = navbar.getElementsByClassName("nav-item");
+// for (var i = 0; i < links.length; i++) {
+//   links[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
 var scrollBrPoint = "80px";
 
 window.onscroll = function(){
